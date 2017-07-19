183dd5df26d161e7268d396e28a509d5c33c9d0b
Михаил Шавин < >
2017-07-19 14:32:24 +0300

    add NP*Z control vector

--- init.m
+++ init.m
@@ -4,7 +4,7 @@ clc
 %% config
 qc.l = 0.2;
 
-% plus
+%% plus
 qc.rays = [1;0;0;
           0;1;0;
           -1;0;0
@@ -45,24 +45,27 @@ qc.I_P = [8.450 0 0
           0 8.450 0
           0 0 4.580]*1e-5;
 
+%% aerodyn
+qc.k = 2.5*1e-5;
+qc.b = 1*1e-6;
+qc.S = 20*30*1e-4;
+qc.c = 1.05;
+qc.ro = 1;
+
+%% control
+jacobian(qc.rays, qc.rot_dirs, qc.k, qc.b, qc.l);
+qc.wmin = abs(qc.m*qc.g(3))/(4*qc.k);
+qc.wrest = sqrt(qc.wmin * 1.2);
+
 %% init pose
 qc.r0 = [0;0;0];
 qc.q0 = [1;0;0;0];
 qc.omega0 = [0;0;1]*0;
-qc.W0 = 1*[1;-1;1;-1]*150^2;
+qc.W0 = 1*[1;-1;1;-1]*qc.wmin;
 qc.Th0 = 1*[45;45;45;45]*pi/180;
 
-%% aerodyn
-qc.k = 2.5*1e-5;
-qc.b = 1*1e-6;
-
-qc.S = 20*30*1e-4;
-qc.c = 1.05;
-qc.ro = 1;
 
 %% lims
 qc.Th_dot_lim = inf;
 qc.der_filter = 8;
 
-%% jacobian
-jacobian(qc.rays, qc.rot_dirs, qc.k, qc.b, qc.l);
