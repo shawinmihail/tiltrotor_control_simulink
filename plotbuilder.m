66df489c6ba253a05f54f56e516446336dad0127
Михаил Шавин < >
2017-07-22 20:52:54 +0300

    control works 0.5!!!

--- plotbuilder.m
+++ plotbuilder.m
@@ -1,12 +1,30 @@
+clc
+
 hold on;
 grid on;
 w = 2;
-t = (0:5 * (1e-4):25);
-plot(t, yout(:,1),'Color','r','LineWidth',w);
-plot(t, yout(:,2),'Color','g','LineWidth',w);
+
+t = time.Data;
+r_r=r_real.Data;
+eul_r = eul_real.Data;
+r_d=r_des.Data;
+eul_d = eul_des.Data;
+
+%% roll
+plot(t, eul_r(:,1),'Color','r','LineWidth',w, 'linestyle', '-');
+plot(t, eul_d(:,1),'Color','k','LineWidth',w, 'linestyle', '--');
+
+%% pitch
+plot(t, eul_r(:,2),'Color','g','LineWidth',w, 'linestyle', '-');
+plot(t, eul_d(:,2),'Color','k','LineWidth',w, 'linestyle', '--');
+
+%% yaw
+plot(t, eul_r(:,3),'Color','b','LineWidth',w, 'linestyle', '-');
+plot(t, eul_d(:,3),'Color','k','LineWidth',w, 'linestyle', '--');
+
+return
 plot(t, yout(:,3),'Color','b','LineWidth',w);
 plot(t, yout(:,4),'Color','m','LineWidth',w);
-
 plot(t, yout(:,5),'Color','r','LineWidth',w, 'linestyle', '--');
 plot(t, yout(:,6),'Color','g','LineWidth',w, 'linestyle', '--');
 plot(t, yout(:,7),'Color','b','LineWidth',w, 'linestyle', '--');
