a28dce7279405d77fbdcf70a26c027be9f3bb606
Михаил Шавин < >
2017-08-03 14:36:34 +0300

    KF v 0.5b

--- kalman_init.m
+++ kalman_init.m
@@ -6,47 +6,43 @@ e33 = eye(3);
 e44 = eye(4);
 
 %% sensors noise
+
 x = clock;
 x = x(6)*1e4;
 qc.seed = mod(x, 31*19*17);
 
-qc.r_snr = 50;
-qc.v_snr = 1000;
-qc.vdot_snr = 1000;
-qc.q_snr = 1000;
-qc.omega_snr = 1000;
+qc.r_snr = 55;
+qc.v_snr = 55;
+qc.vdot_snr = 55;
+qc.q_snr = 55;
+qc.omega_snr = 55;
 % qc.W_snr = 50;
 % qc.Th_snr = 50;
 
 
 %% Kalman
-v_dot0 = [0;0;0];
-omegaB_dot0 = [0;0;0];
-qc.x0 = [qc.r0; qc.v0; v_dot0; qc.qBI0; qc.omegaB0; omegaB_dot0];
-qc.H = [e33 o33 o33 o34 o33 o33;
-        o33 o33 e33 o34 o33 o33;
-        o43 o43 o43 e44 o43 o43;
-        o33 o33 o33 o34 e33 o33];
 
-Qr = 2*1e-6*[1 1 1];
-Qv = 2*1e-2*[1 1 1];
-Qvdot = 1e-6*[1 1 1];
-Qquat = 1e-12*[1 1 1 1];
-Qomega = 1e-12*[1 1 1];
-Qomegadot = 1e-12*[1 1 1];
-qc.Q = diag([Qr Qv Qvdot Qquat Qomega  Qomegadot]);
+% x = r rdot r2dot q omega
+% z = r r2dot q omega
+qc.H = [e33 o33 o33 o34 o33;
+        o33 o33 e33 o34 o33;
+        o43 o43 o43 e44 o43;
+        o33 o33 o33 o34 e33];
 
-Pr = 0*[1 1 1];
-Pv = 0*[1 1 1];
-Pvdot = 0*[1 1 1];
-Pquat = 0*[1 1 1 1];
-Pomega = 0*[1 1 1];
-Pomegadot = 0*[1 1 1];
-qc.P0 = 1*diag([Pr Pv Pvdot Pquat Pomega  Pomegadot]);
+r2dot0 = [0;0;0];
+qc.x0 = [qc.r0; qc.v0; r2dot0; qc.qBI0; qc.omegaB0];
+
+Q_r = 2*1e-5*[1 1 1];
+Q_rdot = 2*1e-3*[1 1 1];
+Q_r2dot = 1e-3*[1 1 1];
+Q_quat = 1e-3*[1 1 1 1];
+Q_omega = 1e-3*[1 1 1];
+qc.Q = diag([Q_r Q_rdot Q_r2dot Q_quat Q_omega]);
+
 qc.P0 = 100*qc.Q;
 
-Rr = 3*1e-6*[1 1 1];
-Rv_dot = 3*1e-4*[1 1 1];
-Rquat = 1e-8*[1 1 1 1];
-Romega = 1e-8*[1 1 1];
-qc.R = diag([Rr Rv_dot Rquat Romega]);
+R_r = 3*1e-6*[1 1 1];
+R_r2dot = 3*1e-2*[1 1 1];
+R_quat = 1e-12*[1 1 1 1];
+R_omega = 1e-12*[1 1 1];
+qc.R = diag([R_r R_r2dot R_quat R_omega]);
