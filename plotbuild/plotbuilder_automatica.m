adb85c29a4c0fdf944f1d8dfae2df31fe6183b2e
shawinmihail <shawinmihail@gmail.com>
2020-10-21 15:48:30 +0300

    remove redundant

--- plotbuild/plotbuilder_automatica.m
+++ plotbuild/plotbuilder_automatica.m
@@ -2,18 +2,18 @@ close all
 clc
 
 path = '/img/';
-save = 1;
+save = 0;
 s = 18;
 dsleg = 0;
 w = 2;
 width = 400;
 height = 600;
 
-run('plotbuilder_traj');
+% run('plotbuilder_traj');
 % run('plotbuilder_ekf_perf');
 % run('plotbuilder_error_rows');
 % run('plotbuilder_ekf_perf_raws');
-% run('plotbuilder_default');
+run('plotbuilder_default');
 % run('plotbuilder_camera');
 % run('plotbuilder_control');
 %run('plotbuilder_error');
